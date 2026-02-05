6d6df706802ac7751b30337d0900d48fcf09ebc6
Marcelo Ruilova <70811785+marceloruilova@users.noreply.github.com>
2026-02-05 16:32:07 -0500

    Fiix jest path alias

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
     '\.(scss|sass|css)$': 'identity-obj-proxy',
   },
   transform: {
